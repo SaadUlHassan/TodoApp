19f55c645b346a919d1bba53a996bc918ae5f16d
Saad Ul Hassan <saad.ul.hussan17@gmail.com>
2022-04-07 23:28:37 +0500

    Revert "Commit Revert + Cookies test"
    This reverts commit 2e4a7eeaa4c9d16fa937212c7bcb6fec1c436d78.

--- src/index.js
+++ src/index.js
@@ -5,22 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./Redux/store";
-if(navigator.cookieEnabled) {
-  ReactDOM.render(
-    <h1>'Please enable cookies'</h1>,
-    document.getElementById("root")
-  );
-}
-
-if(!navigator.cookieEnabled) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <ReduxProvider store={store}>
-        <App />
-      </ReduxProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
