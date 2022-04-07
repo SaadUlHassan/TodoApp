5b82fa8e1b4d31b2875020483e82d8766f7c839a
Saad Ul Hassan <saad.ul.hussan17@gmail.com>
2022-04-07 23:27:41 +0500

    Revert "Cookies test"
    This reverts commit c88e7bbdfe43543470028ffcbf0214c6767ccec1.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./Redux/store";
-if(!navigator.cookieEnabled) {
+if(navigator.cookieEnabled) {
   ReactDOM.render(
     <h1>'Please enable cookies'</h1>,
     document.getElementById("root")
   );
 }
 
-if(navigator.cookieEnabled) {
+if(!navigator.cookieEnabled) {
   ReactDOM.render(
     <React.StrictMode>
       <ReduxProvider store={store}>
